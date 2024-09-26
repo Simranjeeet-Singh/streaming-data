cf8bdc5c3f195aae6fcbd887cab93ef3685db076
Simranjeet Singh <simranjeetsinghc5@gmail.com>
2024-09-26 20:51:31 +0100

    Add API Gateway and Lambda deployment configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,33 @@
-resource "aws_lambda_function" "guardian_lambda" {
-  function_name = "guardian-article-lambda"
-  handler       = "src.main.lambda_handler"
-  runtime       = "python3.8"
-  role          = aws_iam_role.lambda_execution_role.arn
-  filename      = "lambda_package.zip"  # We will create this package later
-
-  environment {
-    variables = {
-      GUARDIAN_API_KEY = "your-api-key-here"
-      SQS_QUEUE_NAME   = aws_sqs_queue.guardian_articles_queue.name
-    }
-  }
+resource "aws_api_gateway_rest_api" "guardian_api" {
+  name = "guardian-api"
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [{
-      "Action": "sts:AssumeRole",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      }
-    }]
-  })
+resource "aws_api_gateway_resource" "articles" {
+  rest_api_id = aws_api_gateway_rest_api.guardian_api.id
+  parent_id   = aws_api_gateway_rest_api.guardian_api.root_resource_id
+  path_part   = "articles"
 }
 
-resource "aws_iam_role_policy" "lambda_execution_policy" {
-  name   = "lambda_execution_policy"
-  role   = aws_iam_role.lambda_execution_role.id
+resource "aws_api_gateway_method" "get_articles" {
+  rest_api_id   = aws_api_gateway_rest_api.guardian_api.id
+  resource_id   = aws_api_gateway_resource.articles.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource": "*"
-      },
-      {
-        "Effect": "Allow",
-        "Action": [
-          "sqs:SendMessage",
-          "sqs:GetQueueUrl"
-        ],
-        "Resource": aws_sqs_queue.guardian_articles_queue.arn
-      }
-    ]
-  })
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.guardian_api.id
+  resource_id = aws_api_gateway_resource.articles.id
+  http_method = aws_api_gateway_method.get_articles.http_method
+  type        = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri         = aws_lambda_function.guardian_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.guardian_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.guardian_api.execution_arn}/*/*"
 }
